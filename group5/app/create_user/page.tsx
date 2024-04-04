820d365f2573189d70eada60a01c28ba23ec636c
Elseter <rking3425@gmail.com>
2024-04-04 19:18:15 -0400

    Create Subpages

--- group5/app/create_user/page.tsx
+++ group5/app/create_user/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to our group project</h1>
-      <h2>This is the DEFAULT page</h2>
+      <h2>This is the CREATE_USER page</h2>
     </div>
   );
 }
